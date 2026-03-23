c2ef61959e24a5432d4d8c19dbd7f772232354f1
Mkn0021 <nayeem21122002@gmail.com>
2026-03-23 20:15:17 +0600

    feat(ui): add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/button";
+import { HeroSection } from "@/components/hero-section";
 import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
