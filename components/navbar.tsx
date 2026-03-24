a4ffb463d366759ede538cfb6b466e0726035ce2
Mkn0021 <nayeem21122002@gmail.com>
2026-03-24 23:14:44 +0600

    refactor: organize code for better maintainibility

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,27 @@
 import Logo from "./ui/logo";
 import { Button } from "./ui/button";
+import Link from "next/link";
+
+const NavLinks = [
+    { name: "About us", href: "#" },
+    { name: "Services", href: "#" },
+    { name: "Use Cases", href: "#" },
+    { name: "Pricing", href: "#" },
+];
 
 export const Navbar = () => (
     <nav className="w-full mt-10 flex items-center justify-between py-2.5 px-25">
         <Logo />
         <div className="flex items-center gap-10 text-[20px]">
-            <a href="#" className="text-black hover:text-gray-700 transition-colors duration-300">
-                About us
-            </a>
-            <a href="#" className="text-black hover:text-gray-700 transition-colors duration-300">
-                Services
-            </a>
-            <a href="#" className="text-black hover:text-gray-700 transition-colors duration-300">
-                Use Cases
-            </a>
-            <a href="#" className="text-black hover:text-gray-700 transition-colors duration-300">
-                Pricing
-            </a>
+            {NavLinks.map((link) => (
+                <Link
+                    key={link.name}
+                    href={link.href}
+                    className="text-black hover:text-gray-700 transition-colors duration-300"
+                >
+                    {link.name}
+                </Link>
+            ))}
             <Button variant="outline">Request a quote</Button>
         </div>
     </nav>
