9d1f2e2531c46b1086059b90253c8f62ead7d568
Mkn0021 <nayeem21122002@gmail.com>
2026-03-25 15:31:42 +0600

    feat(ui): optimize the illustrations and make it more modular

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -24,7 +24,7 @@ export const PlusIcon = () => (
     <svg viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M9.6 25.08V15.36H0V9.72H9.6V0H15.48V9.72H25.08V15.36H15.48V25.08H9.6Z"
-            fill="#191A23"
+            fill="var(--color-accent)"
         />
     </svg>
 );
